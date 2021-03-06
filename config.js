32223939a3aaf5eafa86812a4560057bd1fe3b28
SuperNG6 <sleele@gmail.com>
2021-03-06 18:56:58 +0800

    change api key  63315ea

--- config.js
+++ config.js
@@ -16,10 +16,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'u999301-c32e38c8a0343e764c3b072f',
   ],
 
   // 是否显示监测站点的链接
@@ -33,13 +30,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'sleele的博客',
-      url: 'https://sleele.com'
+      text: '站点状况',
+      url: 'https://status.sleele.com/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/SuperNG6'
     }
   ]
 };
-
